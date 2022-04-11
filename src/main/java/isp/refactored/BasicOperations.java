68513aababf946515b8d87a14b284a1625e30bb9
Roberto Sánchez Arévalo <rsa568422@gmail.com>
2022-04-11 20:21:04 +0200

    Fin de la práctica.
    Se refactoriza el código propuesto al inicio de la práctica de la clase 28 de la sección 9 para cumplir con el principio de ISP.

---
+++ src/main/java/isp/refactored/BasicOperations.java
@@ -0,0 +1,13 @@
+package isp.refactored;
+
+public interface BasicOperations {
+
+    Double add(Double a, Double b);
+
+    Double subtract(Double a, Double b);
+
+    Double multiply(Double a, Double b);
+
+    Double divide(Double a, Double b);
+
+}
---
+++ src/main/java/isp/refactored/TrigonometricOperations.java
@@ -0,0 +1,9 @@
+package isp.refactored;
+
+public interface TrigonometricOperations {
+
+    Double sine(Double angle);
+
+    Double cosine(Double angle);
+
+}
